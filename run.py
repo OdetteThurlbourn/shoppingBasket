84a107f49b6774e8daf92c74808b501151e6d950
OdetteThurlbourn <odette.thurlbourn@gmail.com>
2023-01-12 16:17:25 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Item:
+"""
+Defining class names 'item', the calss has a constructor mathod called `__init__()`.
+Constructor method is automatically called when an object if the calss is created.
+In this instance we are naming the item of clothing and its corrolating price.
+"""
+    def __init__(self, name,, price):
+        self.name = name
+        self.price = name
