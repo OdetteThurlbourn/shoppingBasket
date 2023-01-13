5c032aaf8848e58df2ce47284e7903a6463714dd
OdetteThurlbourn <odette.thurlbourn@gmail.com>
2023-01-13 09:29:28 +0000

    Change out class 'ShoppingBasket' for Basket, add 'self.price' to class

--- run.py
+++ run.py
@@ -1,27 +1,13 @@
-class Item:
+class Basket:
 """
-Defining class names 'item', the class has a constructor method called `__init__()`.
+Defining class names 'Basket', the class has a constructor method called `__init__()`.
 Constructor method is automatically called when an object if the class is created.
 In this instance we are naming the item of clothing and its corrolating price.
 """
-    def __init__(self, name,, price):
-        self.name = name
-        self.price = name
+    def __init__(self, name, price):
+        self.name = {}
+        self.price = {"trousers": 55.50, "t-shirt": 25.50, "bandanna": 12.50}
 
-
-
-class ShoppingBasket:
-"""
-The constructor method takes no parameter and it initializes an empty 
-list attribute called `items` using  the `self` keyword.
-`self` keyword refers to the class (object) being created.
-When a new ShoppingBasket object is created the constructor methos is call and the `items` attribute
-of the object is set to an empty list.
-This class definition can be used to create ShoppingBasket objects that have an empty list of items as an attribute. 
-The empty list will be used to store the items that are added to the basket.
-"""
-    def __init__(self):
-        self.items = []
 
 """
 Defining a method within the ShoppingBasket class called "add_item".
@@ -30,5 +16,11 @@ Taking one parameter "item" and adds the item to the list of items in the Shoppi
 `append` method of list to add parameter "item" to list stored in `items` attribute of the object.
 """
 
-    def add_item(self, item):
-        self.items.append(item)
+    def add_item(self, item, quantity):
+        self.items[item] = quantity
+    
+    def remove_item(self, item):
+        if item in self.items:
+            del self.items[item]
+        else:
+            print(f"Item {item} not found in basket")
