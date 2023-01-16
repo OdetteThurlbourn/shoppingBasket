7fe56bf4ffc7415d416a8b7e9a5e50c74463dca6
OdetteThurlbourn <odette.thurlbourn@gmail.com>
2023-01-16 10:05:24 +0000

    Add user input confirmation of purchase function

--- run.py
+++ run.py
@@ -32,16 +32,47 @@ If "item" is not found it prints the message "Item {item} not found".
             print(f"Item {item} not found in basket")
 
 """
+This function called "view_basket" does not take any parameters. 
+It iterates over the items in the basket and thier quantities.  With each iteration it prints
+the name and the quantity of the item. If the "items" list is empty, 
+it prints a message saying that the basket is empty.
+"""
+    def view_basket(self):
+        if self.items:
+            for item, quantity in self.items.items():
+                print(f"{item}: {quantity}")
+        else:
+            print("Basket is empty.")
+
+
+"""
 This function does not take in any parameter. 
 It checks if the "items" attribute of the object on which this method is called is not empty.
 If NOT empty, it iterates over the items and thier quantities in the "items" list.
 For each iteration it calculates cost of that item by multiplying the quantity of item by its price.
-Keeps adding oct to a variable "total_cost". Then prints item name, quantity and cost. Prints total cost
+Keeps adding cost to a variable "total_cost". Then prints item name, quantity and cost. Prints total cost
 by printing the value of the "total_cost" variable.
 """
-    def view_basket(self):
-        if self.items:
-            for item, quantity in self.item.items():
-                cost = self.prices[item] * quantity
-                total_cost += costprint(f"{item}: {quantity} x {self.price[item]} = {cost}")
-            print(f"Total cost: {total_cost}")
+    def checkout(self):
+        total_cost = 0
+        for item, quantity in self.items.items():
+            cost = self.prices[item] * quantity
+            total_cost += cost
+            print(f"{item}: {quantity} x {self.prices[item]} = {cost}")
+        print(f"Total cost: {total_cost}")
+    
+
+"""
+The function asked the user to confirm the purchase by capturing the input from user using variable named
+"confirm".  If user confirms purchase by typing "yes", it clears the item in the "items" list of the object
+on which this method is called using the `clear()` method, prints a messages "Purchase confirmaed"
+and returns the tptal cost of the purchase. 
+If the user does NOT confirm by typing anything other than "yes", it prints a message "Purchase cancelled".
+"""
+confirm = input("Confirm purchase? (yes/no")
+if confirm == "yes":
+    self.items.clear()
+    print("Purchase confirmed")
+    return total_cost
+else:
+    print("Purchase cancelled.")
